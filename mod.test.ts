9a3e32bd1f8f625dd8981ef6198752bf3dd337b2
David Sherret <dsherret@users.noreply.github.com>
2024-12-14 14:01:26 -0500

    fix: directories should be recursively removed (#1)

--- mod.test.ts
+++ mod.test.ts
@@ -1,4 +1,6 @@
-import { createTempDirSync, createTempFile } from "./mod.ts";
+import { assert } from "@std/assert";
+import { createTempDir, createTempDirSync, createTempFile } from "./mod.ts";
+import { Path } from "@david/path";
 
 Deno.test("readme example", async () => {
   async function tempFileExample() {
@@ -19,4 +21,20 @@ Deno.test("readme example", async () => {
 
   await tempFileExample();
   tempDirExampleSync();
+});
+
+Deno.test("dir dispose should remove dir recursive", async () => {
+  let dir = "";
+  {
+    await using tempDir = await createTempDir();
+    dir = tempDir.toString();
+    tempDir.join("data.txt").writeTextSync("text");
+  }
+  assert(!new Path(dir).existsSync());
+  {
+    using tempDir = createTempDirSync();
+    dir = tempDir.toString();
+    tempDir.join("data.txt").writeTextSync("text");
+  }
+  assert(!new Path(dir).existsSync());
 });
